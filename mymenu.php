0983ec11c3da4ced6d71ffcaa5cf8d3408468cc4
h_dung <h_dung@brycen.com.vn>
2014-09-06 11:45:00 +0700

    Add include

---
+++ mymenu.php
@@ -0,0 +1,57 @@
+<?php
+<div id="header-bottom">
+    <nav>
+        <ul>
+            <li class=" first">
+                <a>Giới thiệu</a>
+                <ul class="level1 submenu">
+                    <li>                        
+                        <a href="http://www.brycen.com.vn/cms/index/lang/vi/id/28">Công ty Brycen Việt Nam</a>
+                    </li>
+                    <li>
+                        <a href="http://www.brycen.com.vn/cms/index/lang/vi/id/22">Chiến lược kinh doanh</a>
+                    </li>
+                </ul>
+
+            </li>
+            <li class="">
+                <a href="http://www.brycen.com.vn/groups/index/lang/vi">
+                       Đội nhóm                                    </a>
+            </li>
+            <!--<li class="">
+                <a href="#">Tin Tức                                    </a>
+            </li>-->
+            <li class="">
+                <a href="http://www.brycen.com.vn/brycenjp/index/lang/vi">Brycen Nhật Bản</a>
+               
+            </li>
+            <!--
+            <li class="">
+                <a href="/events"
+                   >
+                       Sự kiện                                    </a>
+            </li>-->
+			<li class="">
+                <a href="http://www.brycen.com.vn/news/index/lang/vi">
+                    Tin Tức                                    </a>
+            </li>
+            <li class=""><a href="#">Đối tác</a></li>
+             <li class="last ">
+                <a href="http://www.brycen.com.vn/tuyendung">Tuyển dụng</a>
+            </li>
+            <li class="last ">
+                <a href="http://www.brycen.com.vn/contacts">Liên Hệ</a>
+            </li>
+        </ul>
+    </nav>
+    <script type="text/javascript">
+        $("div#header-bottom li").hover(function(){
+            $(this).toggleClass('active'); 
+            $(this).find('ul').toggleClass('hover');
+            
+            
+        })
+    </script>
+</div>
+</div>
+?>
